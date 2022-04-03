41023892acfe51748bd5a49576396d197af0ba76
Petru Valicov <petru.valicov@umontpellier.fr>
2022-04-03 23:18:09 +0200

    test supplémentaire pour Operation

--- src/test/java/fr/umontpellier/iut/expressions/OperationTest.java
+++ src/test/java/fr/umontpellier/iut/expressions/OperationTest.java
@@ -13,4 +13,12 @@ public class OperationTest {
         Expression e = new Operation('*', new Nombre(10), new Nombre(5));
         assertEquals(50, e.calculerValeur());
     }
+
+
+    @Disabled
+    @Test
+    void calculerValeur_2_fois_parenthese_3_plus_1_parenthese() {
+        Expression e = new Operation('*', new Nombre(2), new Operation('+', new Nombre(3), new Nombre(1)));
+        assertEquals(8, e.calculerValeur());
+    }
 }
